2af5a5043daecb08db25c7746cdca303b9bb3ce3
Seng Wee <sengwee_new@hotmail.com>
2019-03-29 23:43:23 +0800

    added custom recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -44,3 +44,11 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'tags', 'time_minutes',
                   'price', 'link',)
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """
+    serialize a recipe detail
+    """
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -61,3 +61,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
         :return:
         """
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """
+        return appropriate serializer class
+        :return:
+        """
+
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
